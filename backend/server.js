ef373c5dc9e9ea6099f057cf7963ad0af5080f5a
soukaina alayoua <salayoua@gmail.com>
2025-05-18 12:49:58 +0100

    update url

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 
 // Public routes
 app.use("/api/auth", authRoutes);
