69cbab44522eca9585f54b51b0d7dace097d1efe
soukaina alayoua <salayoua@gmail.com>
2025-05-18 19:00:55 +0100

    updatemethos in server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
