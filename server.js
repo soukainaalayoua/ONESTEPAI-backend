080b12495487d45bcc1cbf9efb4073c503dfe870
soukaina alayoua <salayoua@gmail.com>
2025-05-18 18:11:44 +0100

    clean server

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ app.use("/api/payment", paymentRoutes);
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 3001;
-app.get("/", (req, res) => {
-  res.send("✅ API is working");
-});
 
 connectDB()
   .then(() => {
